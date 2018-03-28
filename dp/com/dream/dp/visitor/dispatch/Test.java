f7eadedce4b3fc028731e36f40e6477a37be813c
lsl <1039466810@qq.com>
2018-03-28 19:37:54 +0800

    分派模式

---
+++ dp/com/dream/dp/visitor/dispatch/Test.java
@@ -0,0 +1,20 @@
+package com.dream.dp.visitor.dispatch;
+
+/**
+ * 1,静态分配 -- 编译过程中 -- 重载
+ * 2,动态分派 -- 运行时期
+ * 
+ * 方法的调用时根据对象的真实类型而不是静态类型
+ * 
+ * 分派表
+ * @author A170227
+ *
+ */
+public class Test {
+	public static void main(String[] args) {
+		String a = "ab";
+		Object object = a+"c";
+		String b = "abc";
+		System.out.println(object.equals(b));
+	}
+}
